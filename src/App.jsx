6d4290bca45a21122b78b3f270bed45b0c837e7b
brandonst <114836534+b4sleal@users.noreply.github.com>
2022-12-29 14:36:59 -0500

    quick edit to show hibah

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import './App.scss';
+
 const App = () => {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Test</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">Test</h1>
+        </div>
+    );
 };
 
 export default App;
